0e447a2b67e76539d66b7fea3d5e0a0c39893474
Rosie Babbra <rosiebabbra@Rosies-MBP.lan>
2023-08-08 20:53:36 -0500

    Add hobby endpoints

---
+++ backend/api/hobbies/hobbies.py
@@ -0,0 +1,29 @@
+import firebase_admin
+
+from firebase_admin import credentials, firestore
+
+
+def write_interests(db: firestore.client, user_id: int, interests: list):
+
+    query = db.collection('interests').where('user_id', '==', user_id)
+    query_snapshot = query.get()
+
+    for doc in query_snapshot:
+        doc_ref = db.collection('interests').document(doc.id)
+        data_to_update = {
+            'interests': interests
+        }
+        doc_ref.update(data_to_update)
+
+
+def write_noninterests(db: firestore.client, user_id: int, noninterests: list):
+
+    query = db.collection('noninterests').where('user_id', '==', user_id)
+    query_snapshot = query.get()
+
+    for doc in query_snapshot:
+        doc_ref = db.collection('noninterests').document(doc.id)
+        data_to_update = {
+            'noninterests': noninterests
+        }
+        doc_ref.update(data_to_update)
