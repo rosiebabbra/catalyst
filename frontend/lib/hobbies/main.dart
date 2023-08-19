8b37d66afa85d5401f5f0e54e1da67b667f1bdda
Rosie Babbra <rosiebabbra@Rosies-MBP.lan>
2023-08-18 20:08:49 -0500

    Finish logging right and left swipes in db but only with hardcoded vals

--- frontend/lib/hobbies/main.dart
+++ frontend/lib/hobbies/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:swipable_stack/swipable_stack.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
 
 import '../utils/text_fade.dart';
 
@@ -24,6 +26,22 @@ class HobbyScreen extends StatefulWidget {
 
   @override
   HobbyScreenState createState() => HobbyScreenState();
+}
+
+Future<int?> writeSelectedInterest(userId, interestId) async {
+  var response = await http.put(
+      Uri.parse('http://127.0.0.1:8080/selected-interests'),
+      body: {'user_id': userId, 'interest_id': interestId});
+
+  return response.statusCode;
+}
+
+Future<int?> writeDeclinedInterest(userId, interestId) async {
+  var response = await http.put(
+      Uri.parse('http://127.0.0.1:8080/declined-interests'),
+      body: {'user_id': userId, 'interest_id': interestId});
+
+  return response.statusCode;
 }
 
 class HobbyScreenState extends State<HobbyScreen> {
@@ -54,22 +72,19 @@ class HobbyScreenState extends State<HobbyScreen> {
   @override
   Widget build(BuildContext context) {
     List<Card> stack = [];
-    int counter = 0;
-    for (var item in hobbies) {
+    for (int i = 0; i < hobbies.length; i++) {
       stack.add(
         Card(
+          key: Key(i.toString()),
           elevation: 0, // Set elevation to 0 to remove the shadow
           shape: RoundedRectangleBorder(
-            side: BorderSide.none, // Set no borders
-            borderRadius: BorderRadius.circular(0), // Set no border radius
+            borderRadius: BorderRadius.circular(15), // Set no border radius
           ),
-          borderOnForeground: false,
-          color: Colors.white,
+          color: Colors.green,
           surfaceTintColor: Colors.white,
-          shadowColor: Colors.white,
           child: Center(
             child: FadeInText(
-              child: Text(item,
+              child: Text(hobbies[i],
                   style: GoogleFonts.openSans(
                     fontSize: 32,
                     color: Colors.black,
@@ -80,35 +95,57 @@ class HobbyScreenState extends State<HobbyScreen> {
       );
     }
 
+    bool devMode = (Platform.environment['DEV_MODE'] == null) ? false : true;
+
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white,
-        ),
-        body: SizedBox(
-            height: 500,
-            width: 200,
-            child: SwipableStack(
-                detectableSwipeDirections: const {
-                  SwipeDirection.right,
-                  SwipeDirection.left,
-                },
-                controller: _controller,
-                stackClipBehaviour: Clip.none,
-                onSwipeCompleted: (index, direction) {
-                  print('$index, $direction');
-                  // if direction == SwipeDirection.right, add to selected_interests table
-                  // else, add to declined_interests
-                },
-                horizontalSwipeThreshold: 0.8,
-                verticalSwipeThreshold: 0.8,
-                builder: (context, properties) {
-                  final itemIndex = properties.index % stack.length;
+        appBar: devMode
+            ? AppBar(
+                elevation: 0,
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+              )
+            : null,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8,
+                width: MediaQuery.of(context).size.width,
+                child: SwipableStack(
+                  detectableSwipeDirections: const {
+                    SwipeDirection.right,
+                    SwipeDirection.left,
+                  },
+                  controller: _controller,
+                  stackClipBehaviour: Clip.none,
+                  onSwipeCompleted: (index, direction) {
+                    // if direction == SwipeDirection.right, add to selected_interests table
+                    // else, add to declined_interests
+                    if (direction == SwipeDirection.right) {
+                      writeSelectedInterest('1', '8');
+                    } else {
+                      writeDeclinedInterest('1', '1');
+                    }
+                  },
+                  horizontalSwipeThreshold: 0.5,
+                  verticalSwipeThreshold: 0.8,
+                  builder: (context, properties) {
+                    final itemIndex = properties.index % stack.length;
 
-                  return stack[itemIndex];
-                })));
+                    return stack[itemIndex];
+                  },
+                  overlayBuilder: (context, swipeProperty) {
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Opacity(
+                          opacity: swipeProperty.swipeProgress.clamp(0.1, 0.9),
+                          child: Container(color: Colors.pink)),
+                    );
+                  },
+                )),
+          ],
+        ));
   }
 }
 
