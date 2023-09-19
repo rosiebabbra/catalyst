e7a4314c68369f7cfea0e4451a1c0e940269b147
Rosie Babbra <rosiebabbra@gmail.com>
2023-09-19 08:09:38 -0500

    Actually we don't need the intl package

--- frontend/lib/chat/chat_content.dart
+++ frontend/lib/chat/chat_content.dart
@@ -5,7 +5,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:intl/intl.dart';
 
 class ColorConstants {
   static const themeColor = Color(0xfff5a623);
