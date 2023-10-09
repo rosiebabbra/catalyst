35495b5bebd438b21087bd36f3ac1a8aed5313f8
Rosie Babbra <rosiebabbra@gmail.com>
2023-10-09 15:27:55 -0500

    Implement tests for registration screen

--- frontend/test/home_screen_test.dart
+++ frontend/test/home_screen_test.dart
@@ -1,13 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:my_app/home/home_screen.dart';
+import 'package:my_app/onboarding/signup_screen.dart';
 
 void main() {
   testWidgets('Expected home screen elements',
       (WidgetTester widgetTester) async {
-    await widgetTester.pumpWidget(const HomeScreen());
-  });
+    await widgetTester.pumpWidget(const MaterialApp(home: SignupScreen()));
+    await widgetTester.pumpAndSettle();
 
-  // Tap the '+' icon and trigger a frame.
-  // await tester.tap(find.byIcon(Icons.add));
-  // await tester.pump();
+    await widgetTester.enterText(
+        find.byType(TextField).first, 'some-invalid-email');
+    await widgetTester.pumpAndSettle();
+
+    // Press next button
+    await widgetTester.tap(find.byType(TextButton));
+    await widgetTester.pumpAndSettle();
+
+    // Test invalid email address error appears
+    expect(find.text('Please enter a valid email address.'), findsOneWidget);
+  });
 }
