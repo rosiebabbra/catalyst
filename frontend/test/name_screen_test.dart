7a7c7d2f554de2fed3cde4a803ae1473d886feca
Rosie Babbra <rosiebabbra@gmail.com>
2023-10-11 15:26:44 -0500

    Add test for maximum input length

--- frontend/test/name_screen_test.dart
+++ frontend/test/name_screen_test.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:my_app/onboarding/dob_screen.dart';
 import 'package:my_app/onboarding/name_screen.dart';
 import 'package:my_app/onboarding/signup_screen.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
@@ -14,7 +15,7 @@ void main() {
   setUpAll(() async {
     await Firebase.initializeApp();
   });
-  testWidgets('Expected non empty string input',
+  testWidgets('Expect non empty string input',
       (WidgetTester widgetTester) async {
     // Render screen
     await widgetTester.pumpWidget(const MaterialApp(home: NameEntryScreen()));
@@ -29,7 +30,51 @@ void main() {
     await widgetTester.pumpAndSettle();
 
     // Test invalid email address error appears
-    expect(find.text('This is how your name will appear on your profile.'),
+    expect(find.text('Please enter a name to continue.'), findsOneWidget);
+  });
+
+  testWidgets(
+      'More unconventional names can register (looking at you, X Æ A-Xii)',
+      (WidgetTester widgetTester) async {
+    // Render screen
+    await widgetTester
+        .pumpWidget(MaterialApp(home: const NameEntryScreen(), routes: {
+      '/onboarding-dob': (context) => const DOBEntryScreen(),
+    }));
+    await widgetTester.pumpAndSettle();
+
+    // Enter a name with weird characters
+    await widgetTester.enterText(find.byType(TextField).first, 'X Æ A-Xii');
+    await widgetTester.pumpAndSettle();
+
+    // Press next button
+    await widgetTester.tap(find.byType(TextButton));
+    await widgetTester.pumpAndSettle();
+
+    // Test that the next page appears, so that the user was allowed to register
+    expect(find.text('Your birthday?'), findsOneWidget);
+  });
+
+  testWidgets('Surface 50 characters maximum input error',
+      (WidgetTester widgetTester) async {
+    // Render screen
+    await widgetTester
+        .pumpWidget(MaterialApp(home: const NameEntryScreen(), routes: {
+      '/onboarding-dob': (context) => const DOBEntryScreen(),
+    }));
+    await widgetTester.pumpAndSettle();
+
+    // Enter a name with weird characters
+    await widgetTester.enterText(find.byType(TextField).first,
+        'AbCdEfGhIjKlMnOpQrStUvWxYzAaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPp');
+    await widgetTester.pumpAndSettle();
+
+    // Press next button
+    await widgetTester.tap(find.byType(TextButton));
+    await widgetTester.pumpAndSettle();
+
+    // Check that correct error message surfaces
+    expect(find.text('You have exceeded the maximum characters allowed.'),
         findsOneWidget);
   });
 }
