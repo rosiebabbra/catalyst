7f913ad78a4bda17cbd82aa3c93cb6ac74979594
Rosie Babbra <rosiebabbra@gmail.com>
2023-10-12 17:24:43 -0500

    Add navigation test (currently failing)

--- frontend/test/dob_screen_test.dart
+++ frontend/test/dob_screen_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:my_app/onboarding/dob_screen.dart';
+import 'package:my_app/onboarding/gender_identification_screen.dart';
 import 'package:my_app/onboarding/name_screen.dart';
 import 'mock.dart';
 
@@ -39,7 +40,7 @@ void main() {
     await widgetTester.pumpAndSettle();
 
     // Enter almost all digits
-    await widgetTester.enterText(find.byType(TextFormField).first, '0');
+    await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit0);
     await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit4);
     await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit1);
     await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit2);
@@ -54,4 +55,34 @@ void main() {
     expect(
         find.text('Please enter a date in MM-DD-YY format.'), findsOneWidget);
   });
+
+  testWidgets('Navigate to correct next screen',
+      (WidgetTester widgetTester) async {
+    // Render screen
+    await widgetTester.pumpWidget(MaterialApp(
+        home: const DOBEntryScreen(),
+        routes: {
+          '/onboarding-gender': (context) => const GenderIDEntryScreen()
+        }));
+    await widgetTester.pumpAndSettle();
+
+    // Enter almost all digits
+    await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit0);
+    await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit4);
+    await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit1);
+    await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit2);
+    await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit9);
+    await widgetTester.sendKeyEvent(LogicalKeyboardKey.digit8);
+    await widgetTester.pumpAndSettle();
+
+    // Press next button
+    await widgetTester.tap(find.byType(TextButton));
+    await widgetTester.pumpAndSettle();
+
+    // Assert error message surfaces
+    expect(find.text('Which of the following best describes you?'),
+        findsOneWidget);
+  });
+
+  // TODO: Write test to "Expect birth year is calculated correctly"
 }
