65970f23b13b2fa1515aa7f0aa8f7533b23587d4
Rosie Babbra <rosiebabbra@gmail.com>
2023-10-15 19:17:02 -0500

    Get the location detection working when a user configures it in their settings

--- frontend/lib/onboarding/location_services_denied_screen.dart
+++ frontend/lib/onboarding/location_services_denied_screen.dart
@@ -13,118 +13,104 @@ class LocationServiceDeniedScreen extends StatefulWidget {
 }
 
 class LocationServiceDeniedScreenState
-    extends State<LocationServiceDeniedScreen> {
-  var currentStatus;
+    extends State<LocationServiceDeniedScreen> with WidgetsBindingObserver {
+  PermissionStatus currentStatus = PermissionStatus.denied;
+
+  @override
+  Future<void> initState() async {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  Future<LocationPermission> getPermissionStatus() async {
+    LocationPermission status = await Geolocator.requestPermission();
+    return status;
+  }
+
+  @override
+  Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
+    // When the app page is returned to from Settings,
+    // re-check whether permission was granted and set state
+    if (state == AppLifecycleState.resumed) {
+      LocationPermission currentStatus = await getPermissionStatus();
+      if (currentStatus == LocationPermission.always ||
+          currentStatus == LocationPermission.whileInUse) {
+        if (widget.versionId == 'beta') {
+          Navigator.pushNamed(context, '/coming-soon');
+        } else {
+          Navigator.pushNamed(context, '/generating-matches');
+        }
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    Future<PermissionStatus> getPermissionStatus() async {
-      PermissionStatus status = await Permission.location.status;
-      return status;
-    }
-
     return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          showModalBottomSheet(
-            elevation: 200,
-            context: context,
-            builder: (BuildContext context) {
-              return FutureBuilder<PermissionStatus>(
-                future: getPermissionStatus(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    // While the future is still running
-                    return const CircularProgressIndicator();
-                  } else if (snapshot.hasError) {
-                    // If there was an error
-                    return Text('Error: ${snapshot.error}');
-                  } else {
-                    return BottomSheet(
-                      elevation: 200,
-                      onClosing: () async {
-                        var status = await Geolocator.checkPermission();
-                        setState(() {
-                          currentStatus = status;
-                        });
-
-                        if (status == LocationPermission.always ||
-                            status == LocationPermission.whileInUse) {
-                          if (widget.versionId == 'beta') {
-                            Navigator.pushNamed(context, '/coming-soon');
-                          } else {
-                            Navigator.pushNamed(context, '/generating-matches');
-                          }
-                        }
-                      },
-                      builder: (BuildContext context) {
-                        return Padding(
-                          padding: const EdgeInsets.fromLTRB(25, 50, 25, 50),
-                          child: Text(
-                              'Current status: ${currentStatus.toString()}',
-                              style: const TextStyle(fontSize: 18)),
-                        );
-                      },
-                    );
-                  }
-                },
-              );
-            },
-          );
-        },
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          ShaderMask(
-            shaderCallback: (Rect bounds) {
-              return const LinearGradient(
-                colors: [
-                  Color(0xff7301E4),
-                  Color(0xff0E8BFF),
-                  Color(0xff09CBC8),
-                  Color(0xff33D15F),
-                ],
-                stops: [0.0, 0.25, 0.5, 0.75],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-              ).createShader(bounds);
-            },
-            child: const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: FadeInText(
-                child: Text(
-                  'Whoops!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 52.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+      body: FutureBuilder(
+          future: getPermissionStatus(),
+          builder: (context, snapshot) {
+            return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ShaderMask(
+                    shaderCallback: (Rect bounds) {
+                      return const LinearGradient(
+                        colors: [
+                          Color(0xff7301E4),
+                          Color(0xff0E8BFF),
+                          Color(0xff09CBC8),
+                          Color(0xff33D15F),
+                        ],
+                        stops: [0.0, 0.25, 0.5, 0.75],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ).createShader(bounds);
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: FadeInText(
+                        child: Text(
+                          'Whoops!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 52.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-          ),
-          FadeInText(
-            delayStart: const Duration(milliseconds: 500),
-            child: Text(
-              'Location access is required.',
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[900]),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(25),
-            child: FadeInText(
-              delayStart: const Duration(seconds: 1),
-              child: Text(
-                'Please allow permissions in your app settings and return to continue.',
-                style: TextStyle(fontSize: 16, color: Colors.grey[800]),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-        ]),
-      ),
+                  FadeInText(
+                    delayStart: const Duration(milliseconds: 500),
+                    child: Text(
+                      'Location access is required.',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[900]),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(25),
+                    child: FadeInText(
+                      delayStart: const Duration(seconds: 1),
+                      child: Text(
+                        'Please allow permissions in your app settings and return to continue.',
+                        style: TextStyle(fontSize: 16, color: Colors.grey[800]),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ]);
+          }),
     );
   }
 }
