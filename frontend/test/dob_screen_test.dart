c3ed35004698b7f022a84268e7abb9aeedb53259
Rosie Babbra <rosiebabbra@gmail.com>
2023-10-15 23:19:19 -0500

    Remove old DOB test code since we switched to CupertinoDatePicker widget

--- frontend/test/dob_screen_test.dart
+++ frontend/test/dob_screen_test.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:my_app/onboarding/dob_screen.dart';
 import 'mock.dart';
 
 void main() {
@@ -12,16 +10,8 @@ void main() {
   });
 
   testWidgets('Surface error if only one date digit is input',
-      (WidgetTester widgetTester) async {
-    // Render screen
-    await widgetTester.pumpWidget(const MaterialApp(home: DOBEntryScreen()));
-    await widgetTester.pumpAndSettle();
-  });
+      (WidgetTester widgetTester) async {});
 
   testWidgets('Surface error if almost all date digits are input',
-      (WidgetTester widgetTester) async {
-    // Render screen
-    await widgetTester.pumpWidget(const MaterialApp(home: DOBEntryScreen()));
-    await widgetTester.pumpAndSettle();
-  });
+      (WidgetTester widgetTester) async {});
 }
