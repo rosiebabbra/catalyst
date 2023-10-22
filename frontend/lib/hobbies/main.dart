cda9e6134da268a4d6fb41dde10c89ffe83700ce
Rosie Babbra <rosiebabbra@gmail.com>
2023-10-22 18:59:12 -0500

    Get the stack working with the data from firestore

--- frontend/lib/hobbies/main.dart
+++ frontend/lib/hobbies/main.dart
@@ -1,69 +1,66 @@
 // ignore_for_file: must_be_immutable
 
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_app/chat/chat_list.dart';
 import 'package:my_app/matches/match_screen.dart';
 import 'package:my_app/my_profile/my_profile_screen.dart';
 import 'package:swipable_stack/swipable_stack.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import '../utils/text_fade.dart';
 
 class HobbyScreen extends StatefulWidget {
-  String hobby;
-  HobbyScreen({super.key, required this.hobby});
+  const HobbyScreen({super.key});
 
   @override
   HobbyScreenState createState() => HobbyScreenState();
 }
 
-Future<String> getInterests() async {
-  var response = await http.get(Uri.parse('http://127.0.0.1:8080/interests'));
+class InterestModel {
+  final String interest;
+  final String interestDesc;
 
-  return response.body;
+  InterestModel({required this.interest, required this.interestDesc});
+
+  factory InterestModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> snapshot) {
+    return InterestModel(
+      interest: snapshot.data()?['interest'] ?? '',
+      interestDesc: snapshot.data()?['interest_desc'] ?? '',
+    );
+  }
 }
 
-Future<int?> writeSelectedInterest(userId, interestId) async {
-  var response = await http.put(
-      Uri.parse('http://127.0.0.1:8080/selected-interests'),
-      body: {'user_id': userId, 'interest_id': interestId});
+Future<List<Map<String, dynamic>>> getInterests() async {
+  QuerySnapshot<Map<String, dynamic>> querySnapshot =
+      await FirebaseFirestore.instance.collection('your_collection').get();
 
-  return response.statusCode;
-}
+  List<Map<String, dynamic>> dataList = [];
+  for (QueryDocumentSnapshot<Map<String, dynamic>> document
+      in querySnapshot.docs) {
+    // Assuming 'column1' and 'column2' are the names of the two columns
+    String interest = document.data()['interest'];
+    String value2 = document.data()['interest_desc'];
 
-Future<int?> writeDeclinedInterest(userId, interestId) async {
-  var response = await http.put(
-      Uri.parse('http://127.0.0.1:8080/declined-interests'),
-      body: {'user_id': userId, 'interest_id': interestId});
+    dataList.add({'interest': interest, 'column2': value2});
+  }
 
-  return response.statusCode;
+  return dataList;
 }
 
 class HobbyScreenState extends State<HobbyScreen> {
-  List<dynamic> hobbies = [];
   late final SwipableStackController _controller;
   void _listenController() => setState(() {});
   int currentNavbarIndex = 0;
-
-  Future<List> serializeInterestsList() async {
-    var result = await getInterests();
-    var interests = jsonDecode(result);
-    return interests;
-  }
-
-  void populateList() async {
-    hobbies = await serializeInterestsList();
-    setState(() {});
-  }
+  List<dynamic> hobbies = [];
 
   @override
   void initState() {
     super.initState();
     _controller = SwipableStackController()..addListener(_listenController);
-    populateList();
   }
 
   @override
@@ -76,53 +73,6 @@ class HobbyScreenState extends State<HobbyScreen> {
 
   @override
   Widget build(BuildContext context) {
-    List<Card> stack = [];
-    for (int i = 0; i < hobbies.length; i++) {
-      stack.add(
-        Card(
-          elevation: 0,
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ShaderMask(
-                shaderCallback: (Rect bounds) {
-                  return const LinearGradient(
-                    colors: [
-                      Color(0xff7301E4),
-                      Color(0xff0E8BFF),
-                      Color(0xff09CBC8),
-                      Color(0xff33D15F),
-                    ],
-                    stops: [0.0, 0.25, 0.5, 0.75],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                  ).createShader(bounds);
-                },
-                child: Text(hobbies[i]['interest'],
-                    style: GoogleFonts.openSans(
-                        fontSize: 40,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center),
-              ),
-              FadeInText(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(hobbies[i]['interest_desc'],
-                      style: GoogleFonts.openSans(
-                        fontSize: 16,
-                        color: Colors.blueGrey,
-                      ),
-                      textAlign: TextAlign.center),
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-
     bool devMode = (Platform.environment['DEV_MODE'] == null) ? false : true;
 
     void onTabTapped(int index) {
@@ -132,7 +82,7 @@ class HobbyScreenState extends State<HobbyScreen> {
     }
 
     final List<Widget> pages = [
-      interestSwipe(context, stack),
+      interestSwipe(context, []),
       const MatchScreen(userId: 1),
       const ChatList(),
       const MyProfileScreen()
@@ -163,70 +113,53 @@ class HobbyScreenState extends State<HobbyScreen> {
         body: pages[currentNavbarIndex]);
   }
 
-  Column interestSwipe(BuildContext context, List<Card> stack) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-            height: MediaQuery.of(context).size.height * 0.8,
-            width: MediaQuery.of(context).size.width,
-            child: SwipableStack(
-              detectableSwipeDirections: const {
-                SwipeDirection.right,
-                SwipeDirection.left,
-              },
-              controller: _controller,
-              stackClipBehaviour: Clip.none,
-              onSwipeCompleted: (index, direction) {
-                if (direction == SwipeDirection.right) {
-                  writeSelectedInterest(
-                      '4', hobbies[index]['interest_id'].toString());
-                } else if (direction == SwipeDirection.left) {
-                  writeDeclinedInterest(
-                      '1', hobbies[index]['interest_id'].toString());
-                }
-              },
-              horizontalSwipeThreshold: 0.5,
-              verticalSwipeThreshold: 0.8,
-              builder: (context, properties) {
-                if (stack.isEmpty) {
-                  return const UnconstrainedBox(
-                    child: SizedBox(
-                        height: 100,
-                        width: 100,
-                        child: CircularProgressIndicator(
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Color(0xff33D15F)),
-                          strokeWidth: 8,
-                        )),
-                  );
-                }
-                final itemIndex = properties.index % stack.length;
+  interestSwipe(BuildContext context, List<Card> stack) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('interests').snapshots(),
+      builder: (context,
+          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
 
-                return stack[itemIndex];
-              },
-              overlayBuilder: (context, swipeProperty) {
-                Color swipeColor =
-                    swipeProperty.direction == SwipeDirection.right
-                        ? const Color(0xff33D15F)
-                        : const Color(0xff7301E4);
-                return Opacity(
-                    opacity: swipeProperty.swipeProgress.clamp(0, 0.6),
-                    child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                        color: swipeColor));
-              },
-            )),
-      ],
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+
+        List<InterestModel> data = snapshot.data?.docs
+                .map((DocumentSnapshot<Map<String, dynamic>> doc) =>
+                    InterestModel.fromSnapshot(doc))
+                .toList() ??
+            [];
+
+        return SwipableStack(
+          itemCount: data.length,
+          builder: (context, properties) {
+            final int index = properties.index;
+
+            // try {
+            //   index = properties.index % hobbies.length;
+            // } catch (e) {
+            //   index = 0;
+            // }
+
+            if (index >= data.length) {
+              return Container();
+            }
+
+            return Card(
+              child: ListTile(
+                title: Text(data[index].interestDesc),
+                subtitle: Text(data[index].interest),
+              ),
+            );
+          },
+          onSwipeCompleted: (index, direction) {
+            // Handle swipe completion if needed
+            print('Swiped $direction on item $index');
+          },
+        );
+      },
     );
   }
-}
-
-class Hobby {
-  int id;
-  String name;
-
-  // Constructor
-  Hobby({required this.id, required this.name});
 }
