745e84a5aed15f1c46cd1f20f6b18611184a0498
Rosie Babbra <rosiebabbra@gmail.com>
2023-11-05 18:12:58 -0600

    Begin pulling match screen data from db and programmatically generating the carousel pages

--- frontend/lib/utils/utils.dart
+++ frontend/lib/utils/utils.dart
@@ -1,7 +1,41 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 bool isSafeFromSqlInjection(String input) {
   RegExp sqlPattern = RegExp(
     r"(\b(union|select|insert|update|delete|drop|alter)\b)|(--\s|/\*|\*/)",
     caseSensitive: false,
   );
   return !sqlPattern.hasMatch(input);
+}
+
+double calculateDistance(GeoPoint point1, GeoPoint point2) {
+  const earthRadiusMiles = 3958.8; // Radius of the Earth in miles
+
+  double radians(double degrees) {
+    return degrees * (pi / 180);
+  }
+
+  num haversine(double a, double b) {
+    return pow(sin((b - a) / 2), 2);
+  }
+
+  double haversineDistance() {
+    double lat1Rad = radians(point1.latitude);
+    double lon1Rad = radians(point1.longitude);
+    double lat2Rad = radians(point2.latitude);
+    double lon2Rad = radians(point2.longitude);
+
+    double dLat = lat2Rad - lat1Rad;
+    double dLon = lon2Rad - lon1Rad;
+
+    double a = haversine(dLat, dLat) +
+        cos(lat1Rad) * cos(lat2Rad) * haversine(dLon, dLon);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+    return earthRadiusMiles * c;
+  }
+
+  return haversineDistance();
 }
