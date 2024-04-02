dd47a0582da4b088e2c4930cd8ad5e591abd9343
Rosie Babbra <rosiebabbra@gmail.com>
2024-04-02 14:08:45 -0500

    Get all the messages, sent and received

--- frontend/lib/chat/chat_content.dart
+++ frontend/lib/chat/chat_content.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
+import 'package:rxdart/rxdart.dart';
 
 convertTimestampToTime(timestamp) {
   var dt = timestamp.toDate();
@@ -27,13 +29,43 @@ class ChatContentState extends State<ChatContent> {
 
   @override
   Widget build(BuildContext context) {
-    Stream<QuerySnapshot<Map<String, dynamic>>> querySnapshot =
+    Stream<QuerySnapshot<Map<String, dynamic>>> receivedQuerySnapshot =
         FirebaseFirestore.instance
             .collection('messages')
             .where('sender_id', isEqualTo: widget.senderData['senderId'])
             .where('receiver_id', isEqualTo: widget.receiverId)
-            .orderBy('timestamp')
             .snapshots();
+
+    Stream<QuerySnapshot<Map<String, dynamic>>> sentQuerySnapshot =
+        FirebaseFirestore.instance
+            .collection('messages')
+            .where('sender_id', isEqualTo: widget.receiverId)
+            .where('receiver_id', isEqualTo: widget.senderData['senderId'])
+            .snapshots();
+
+    Stream<List<QueryDocumentSnapshot<Map<String, dynamic>>>> messageStream =
+        CombineLatestStream.list([receivedQuerySnapshot, sentQuerySnapshot])
+            .map((List<dynamic> snapshotList) {
+      // Cast each dynamic element to QuerySnapshot<Map<String, dynamic>>
+      List<QuerySnapshot<Map<String, dynamic>>> snapshots = snapshotList
+          .map((dynamic snapshot) =>
+              snapshot as QuerySnapshot<Map<String, dynamic>>)
+          .toList();
+
+      // Merge documents from both snapshots into a single list
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> allDocuments =
+          snapshots.expand((snapshot) => snapshot.docs).toList();
+
+      // Sort the combined list by timestamp
+      allDocuments.sort((a, b) {
+        DateTime aTimestamp = a.data()['timestamp'].toDate();
+        DateTime bTimestamp = b.data()['timestamp'].toDate();
+        return aTimestamp
+            .compareTo(bTimestamp); // Use compareTo for ascending order
+      });
+
+      return allDocuments;
+    });
 
     TextEditingController messageController = TextEditingController();
     final FirebaseAuth auth = FirebaseAuth.instance;
@@ -54,10 +86,10 @@ class ChatContentState extends State<ChatContent> {
         body: Column(children: [
           Expanded(
             flex: 4,
-            child: StreamBuilder(
-                stream: querySnapshot,
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
+            child: StreamBuilder<
+                    List<QueryDocumentSnapshot<Map<String, dynamic>>>>(
+                stream: messageStream,
+                builder: (context, snapshot) {
                   if (snapshot.hasError) {
                     return Text(snapshot.error.toString());
                   }
@@ -65,8 +97,9 @@ class ChatContentState extends State<ChatContent> {
                   if (!snapshot.hasData) {
                     return const CircularProgressIndicator();
                   } else {
-                    var msgList = snapshot.data!.docs.toList();
+                    var msgList = snapshot.data!;
                     return ListView.builder(
+                      key: Key("${Random().nextDouble()}"),
                       itemCount: msgList.length,
                       itemBuilder: (context, index) {
                         DocumentSnapshot document = msgList[index];
@@ -78,12 +111,12 @@ class ChatContentState extends State<ChatContent> {
 
                         return Padding(
                           padding: data['receiver_id'] == currentUserId
-                              ? const EdgeInsets.fromLTRB(200, 0, 0, 0)
-                              : const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                              ? const EdgeInsets.fromLTRB(15, 0, 200, 0)
+                              : const EdgeInsets.fromLTRB(200, 0, 15, 0),
                           child: Card(
                             color: data['receiver_id'] == currentUserId
-                                ? const Color(0xff7301E4)
-                                : Colors.grey[200],
+                                ? Colors.grey[200]
+                                : const Color(0xff7301E4),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(20),
                             ),
@@ -96,18 +129,21 @@ class ChatContentState extends State<ChatContent> {
                                         padding: const EdgeInsets.fromLTRB(
                                             0, 10, 0, 0),
                                         child: Text(data['content'].toString(),
+                                            key:
+                                                Key("${Random().nextDouble()}"),
                                             style: TextStyle(
                                                 color: data['receiver_id'] ==
                                                         currentUserId
-                                                    ? Colors.white
-                                                    : Colors.black)),
+                                                    ? Colors.black
+                                                    : Colors.white)),
                                       )
                                     : Text(data['content'].toString(),
+                                        key: Key("${Random().nextDouble()}"),
                                         style: TextStyle(
                                             color: data['receiver_id'] ==
                                                     currentUserId
-                                                ? Colors.white
-                                                : Colors.black)),
+                                                ? Colors.black
+                                                : Colors.white)),
                               ),
                               onTap: () {
                                 setState(() {
@@ -132,8 +168,8 @@ class ChatContentState extends State<ChatContent> {
                                           style: TextStyle(
                                               color: data['receiver_id'] ==
                                                       currentUserId
-                                                  ? Colors.white
-                                                  : Colors.black,
+                                                  ? Colors.black
+                                                  : Colors.white,
                                               fontWeight: FontWeight.bold)),
                                     )
                                   : Container(),
