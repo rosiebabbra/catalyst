700450e5195bb3a187d9d8beaf72828bb10a55ee
Rosie Babbra <rosiebabbra@gmail.com>
2024-04-03 10:24:18 -0500

    Reorganize code

--- frontend/lib/hobbies/main.dart
+++ frontend/lib/hobbies/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +7,7 @@ import 'package:my_app/chat/chat_list.dart';
 import 'package:my_app/my_profile/my_profile_screen.dart';
 import 'package:swipable_stack/swipable_stack.dart';
 import '../utils/text_fade.dart';
+import 'package:gradient_circular_progress_indicator/gradient_circular_progress_indicator.dart';
 
 class InterestModel {
   final String interest;
@@ -85,157 +88,156 @@ class HobbyScreenState extends State<HobbyScreen> {
       ..dispose();
   }
 
+  void onTabTapped(int index) {
+    setState(() {
+      currentNavbarIndex = index;
+    });
+  }
+
+  Future<Set> fetchExclusionList() async {
+    final selectedInterests =
+        await FirebaseFirestore.instance.collection('selected_interests').get();
+    final declinedInterests =
+        await FirebaseFirestore.instance.collection('declined_interests').get();
+
+    Set<dynamic> exclusions = {
+      ...selectedInterests.docs.expand((doc) => doc['interest_ids']).toSet(),
+      ...declinedInterests.docs.expand((doc) => doc['interest_ids']).toSet(),
+    };
+
+    return exclusions;
+  }
+
+  interestSwipe(BuildContext context) {
+    return FutureBuilder(
+        future: fetchExclusionList(),
+        builder: (context, snapshot) {
+          final exclusionList = snapshot.data;
+
+          return StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('interests').snapshots(),
+            builder: (context,
+                AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator(
+                    color: Color(0xff7301E4));
+              }
+
+              if (!snapshot.hasData) {
+                return const CircularProgressIndicator(
+                    color: Color(0xff7301E4));
+              }
+
+              final docs = snapshot.data?.docs
+                  .where((doc) =>
+                      exclusionList != null &&
+                      !exclusionList.contains(doc['interest_id']))
+                  .toList();
+
+              if (docs!.isEmpty) {
+                return const UnconstrainedBox(
+                  child: SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            AlwaysStoppedAnimation<Color>(Color(0xff33D15F)),
+                        strokeWidth: 8,
+                      )),
+                );
+              }
+
+              return SwipableStack(
+                horizontalSwipeThreshold: 0.5,
+                verticalSwipeThreshold: 0.8,
+                itemCount: docs.isNotEmpty ? docs.length : 0,
+                stackClipBehaviour: Clip.none,
+                overlayBuilder: (context, swipeProperty) {
+                  Color swipeColor =
+                      swipeProperty.direction == SwipeDirection.right
+                          ? const Color(0xff33D15F)
+                          : const Color(0xff7301E4);
+                  return Opacity(
+                      opacity: swipeProperty.swipeProgress.clamp(0, 0.6),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.0),
+                            color: swipeColor),
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                      ));
+                },
+                builder: (context, properties) {
+                  final int index = properties.index;
+
+                  if (index >= docs.length) {
+                    return Container();
+                  }
+
+                  return Card(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ShaderMask(
+                            shaderCallback: (Rect bounds) {
+                              return const LinearGradient(
+                                colors: [
+                                  Color(0xff7301E4),
+                                  Color(0xff0E8BFF),
+                                  Color(0xff09CBC8),
+                                  Color(0xff33D15F),
+                                ],
+                                stops: [0.0, 0.25, 0.5, 0.75],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                              ).createShader(bounds);
+                            },
+                            child: FittedBox(
+                              child: Text(docs[index]['interest'],
+                                  style: const TextStyle(
+                                    fontSize: 36.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  )),
+                            )),
+                        FadeInText(
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(docs[index]['interest_desc'],
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 16.0,
+                                )),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+                onSwipeCompleted: (index, direction) {
+                  final FirebaseAuth auth = FirebaseAuth.instance;
+                  final User? user = auth.currentUser;
+                  if (direction == SwipeDirection.right) {
+                    writeInterest(
+                        user, 'selected_interests', docs[index]['interest_id']);
+                  } else if (direction == SwipeDirection.left) {
+                    writeInterest(
+                        user, 'declined_interests', docs[index]['interest_id']);
+                  }
+                },
+              );
+            },
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    void onTabTapped(int index) {
-      setState(() {
-        currentNavbarIndex = index;
-      });
-    }
-
-    Future<Set> fetchExclusionList() async {
-      final selectedInterests = await FirebaseFirestore.instance
-          .collection('selected_interests')
-          .get();
-      final declinedInterests = await FirebaseFirestore.instance
-          .collection('declined_interests')
-          .get();
-
-      Set<dynamic> exclusions = {
-        ...selectedInterests.docs.expand((doc) => doc['interest_ids']).toSet(),
-        ...declinedInterests.docs.expand((doc) => doc['interest_ids']).toSet(),
-      };
-
-      return exclusions;
-    }
-
-    interestSwipe(BuildContext context) {
-      return FutureBuilder(
-          future: fetchExclusionList(),
-          builder: (context, snapshot) {
-            final exclusionList = snapshot.data;
-
-            return StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection('interests')
-                  .snapshots(),
-              builder: (context,
-                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                }
-
-                if (!snapshot.hasData) {
-                  return const CircularProgressIndicator();
-                }
-
-                final docs = snapshot.data?.docs
-                    .where((doc) =>
-                        exclusionList != null &&
-                        !exclusionList.contains(doc['interest_id']))
-                    .toList();
-
-                if (docs!.isEmpty) {
-                  return const UnconstrainedBox(
-                    child: SizedBox(
-                        height: 100,
-                        width: 100,
-                        child: CircularProgressIndicator(
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Color(0xff33D15F)),
-                          strokeWidth: 8,
-                        )),
-                  );
-                }
-
-                return SwipableStack(
-                  horizontalSwipeThreshold: 0.5,
-                  verticalSwipeThreshold: 0.8,
-                  itemCount: docs.isNotEmpty ? docs.length : 0,
-                  stackClipBehaviour: Clip.none,
-                  overlayBuilder: (context, swipeProperty) {
-                    Color swipeColor =
-                        swipeProperty.direction == SwipeDirection.right
-                            ? const Color(0xff33D15F)
-                            : const Color(0xff7301E4);
-                    return Opacity(
-                        opacity: swipeProperty.swipeProgress.clamp(0, 0.6),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12.0),
-                              color: swipeColor),
-                          height: MediaQuery.of(context).size.height,
-                          width: MediaQuery.of(context).size.width,
-                        ));
-                  },
-                  builder: (context, properties) {
-                    final int index = properties.index;
-
-                    if (index >= docs.length) {
-                      return Container();
-                    }
-
-                    return Card(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ShaderMask(
-                              shaderCallback: (Rect bounds) {
-                                return const LinearGradient(
-                                  colors: [
-                                    Color(0xff7301E4),
-                                    Color(0xff0E8BFF),
-                                    Color(0xff09CBC8),
-                                    Color(0xff33D15F),
-                                  ],
-                                  stops: [0.0, 0.25, 0.5, 0.75],
-                                  begin: Alignment.centerLeft,
-                                  end: Alignment.centerRight,
-                                ).createShader(bounds);
-                              },
-                              child: FittedBox(
-                                child: Text(docs[index]['interest'],
-                                    style: const TextStyle(
-                                      fontSize: 36.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    )),
-                              )),
-                          FadeInText(
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text(docs[index]['interest_desc'],
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                    fontSize: 16.0,
-                                  )),
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                  onSwipeCompleted: (index, direction) {
-                    final FirebaseAuth auth = FirebaseAuth.instance;
-                    final User? user = auth.currentUser;
-                    if (direction == SwipeDirection.right) {
-                      writeInterest(user, 'selected_interests',
-                          docs[index]['interest_id']);
-                    } else if (direction == SwipeDirection.left) {
-                      writeInterest(user, 'declined_interests',
-                          docs[index]['interest_id']);
-                    }
-                  },
-                );
-              },
-            );
-          });
-    }
-
     final List<Widget> pages = [
       interestSwipe(context),
       const ChatList(userIds: ['abc']),
